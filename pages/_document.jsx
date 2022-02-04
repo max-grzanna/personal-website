26b7574dbc308721648bbc36999cdfceef54ca10
Max <grzannamax@gmail.com>
2022-02-04 20:20:02 +0100

    added plausible analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,7 +7,13 @@ import {
 export default function Document(props) {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          data-domain="max-grzanna.tech"
+          src="https://www.max-grzanna-analytics.xyz/js/plausible.js"
+        />
+      </Head>
       <body className="bg-white dark:bg-ebony text-gray-600 dark:text-gray-100">
         <Main />
         <NextScript />

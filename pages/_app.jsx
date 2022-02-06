01517f334510bbe38a89aba7ca3b2fc0f303e7f4
Max <grzannamax@gmail.com>
2022-02-06 22:44:54 +0100

    added script to _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system">
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: ` 
+(function(f, a, t, h, o, m){
+\ta[h]=a[h]||function(){
+\t\t(a[h].q=a[h].q||[]).push(arguments)
+\t};
+\to=f.createElement('script'),
+\tm=f.getElementsByTagName('script')[0];
+\to.async=1; o.src=t; o.id='fathom-script';
+\tm.parentNode.insertBefore(o,m)
+})(document, window, '//max-grzanna-analytics.xyz/tracker.js', 'fathom');
+fathom('set', 'siteId', 'WEHXY');
+fathom('trackPageview');
+  `,
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,28 +10,7 @@ import Script from 'next/script'
 export default function Document(props) {
     return (
         <Html lang="en">
-            <Head>
-                <Script
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: ` 
-(function(f, a, t, h, o, m){
-\ta[h]=a[h]||function(){
-\t\t(a[h].q=a[h].q||[]).push(arguments)
-\t};
-\to=f.createElement('script'),
-\tm=f.getElementsByTagName('script')[0];
-\to.async=1; o.src=t; o.id='fathom-script';
-\tm.parentNode.insertBefore(o,m)
-})(document, window, '//max-grzanna-analytics.xyz/tracker.js', 'fathom');
-fathom('set', 'siteId', 'WEHXY');
-fathom('trackPageview');
-  `,
-                    }}
-                />
-
-
-            </Head>
+            <Head/>
             <body className="bg-white dark:bg-ebony text-gray-600 dark:text-gray-100">
             <Main/>
             <NextScript/>

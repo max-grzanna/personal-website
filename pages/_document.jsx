4defe7782b253e51567d8deddc8920b121db97cb
Max <grzannamax@gmail.com>
2022-02-06 22:25:52 +0100

    added fathom analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,23 +1,36 @@
+/* eslint-disable */
 import React from 'react';
 import {
-  Html, Head, Main, NextScript,
+    Html, Head, Main, NextScript,
 } from 'next/document';
+import Script from 'next/script'
+
 
 // eslint-disable-next-line no-unused-vars
 export default function Document(props) {
-  return (
-    <Html lang="en">
-      <Head>
-        <script
-          defer
-          data-domain="max-grzanna.tech"
-          src="https://www.max-grzanna-analytics.xyz/js/plausible.js"
-        />
-      </Head>
-      <body className="bg-white dark:bg-ebony text-gray-600 dark:text-gray-100">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <Script id="show-banner" strategy="beforeInteractive">
+                    {`(function(f, a, t, h, o, m){
+\ta[h]=a[h]||function(){
+\t\t(a[h].q=a[h].q||[]).push(arguments)
+\t};
+\to=f.createElement('script'),
+\tm=f.getElementsByTagName('script')[0];
+\to.async=1; o.src=t; o.id='fathom-script';
+\tm.parentNode.insertBefore(o,m)
+})(document, window, '//max-grzanna-analytics.xyz/tracker.js', 'fathom');
+fathom('set', 'siteId', 'WEHXY');
+fathom('trackPageview');`}
+                </Script>
+
+
+            </Head>
+            <body className="bg-white dark:bg-ebony text-gray-600 dark:text-gray-100">
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    );
 }

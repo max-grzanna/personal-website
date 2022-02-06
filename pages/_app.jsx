3f23d05d4cfb649be33826505c4c4a329b99b12c
Max <grzannamax@gmail.com>
2022-02-07 00:31:30 +0100

    Revert "cookie consent stage 1"
    This reverts commit eb5ae09bf0240d472785acea5203e47a692c0afe.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,53 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
 import Script from 'next/script';
-import CookieConsent from 'react-cookie-consent';
 
 function MyApp({ Component, pageProps }) {
-  const [cookieIsSet, setCookieIsSet] = useState(false);
-
   return (
     <ThemeProvider attribute="class" defaultTheme="system">
-      <CookieConsent
-        onAccept={() => {
-          setCookieIsSet(!cookieIsSet);
-        }}
-        buttonText="Continue to website"
-        overlay
-        style={{
-          background: 'white',
-          color: 'black',
-          width: '350px',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          borderRadius: '1rem',
-          padding: '1rem',
-        }}
-        buttonStyle={{
-          border: '0px',
-          background: 'none',
-          borderRadius: '0px',
-          boxShadow: 'none',
-          color: 'black',
-          cursor: 'pointer',
-          flex: '0 0 auto',
-          fontSize: '13px',
-          margin: 'none',
-          padding: '1rem',
-        }}
-      >
-        This website uses cookies.
-        {' '}
-        <br />
-        <span style={{ fontSize: '10px' }}>By continuing to browse on this website, you agree to our use of cookies.</span>
-      </CookieConsent>
-      {cookieIsSet === true
-                && (
-                <Script
-                  strategy="afterInteractive"
-                  dangerouslySetInnerHTML={{
-                    __html: ` 
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: ` 
 (function(f, a, t, h, o, m){
 \ta[h]=a[h]||function(){
 \t\t(a[h].q=a[h].q||[]).push(arguments)
@@ -60,9 +22,8 @@ function MyApp({ Component, pageProps }) {
 fathom('set', 'siteId', 'WEHXY');
 fathom('trackPageview');
   `,
-                  }}
-                />
-                )}
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
